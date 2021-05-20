ac94d1de2546f2d52c9119f301752c2e7d882417
Christopher Hebert <hebert.christopherj@gmail.com>
2021-05-20 10:00:30 -0400

    small cleanups and added doc comments

--- token.c
+++ token.c
@@ -30,54 +30,99 @@ struct Token {
   const u8* source;
   u64 length;
 };
-void PrintToken(struct Token* token);
-const u8 *TokenTypeString(enum TokenType type);
 
 // Reads from source. The next token is stored in result.
 // The unconsumed portion of source returned.
 const u8 *NextToken(const u8 *source, struct Token *result);
 
-const u8 *NextNumberOrSymbolOrPairSeparatorToken(const u8 *source, struct Token *result);
+
+// Return the string representing the token type.
+const u8 *TokenTypeString(enum TokenType type);
+// Debug print of token
+void PrintToken(struct Token* token);
+
+// Reads the next number or symbol token from source until
 const u8 *NextNumberOrSymbolToken(const u8 *source, struct Token *result);
+// Reads the next string token from source
 const u8 *NextStringToken(const u8 *source, struct Token *result);
+// Reads the next line comment token from source
 const u8 *NextLineCommentToken(const u8 *source, struct Token *result);
+
+// Reads all initial whitespace from source and discards it.
 const u8 *DiscardInitialWhitespace(const u8 *source);
+// If an escape is encountered, extends token with it and the next character.
 const u8 *HandleEscape(const u8 *source, struct Token *result);
 
+// Consumes 0+ digits and returns the number encountered.
+u64 CountDigits(const u8 **source, u64 *length);
+// Consumes an optional number sign.
+void OptionalNumberSign(const u8 **source, u64 *length);
+// Consumes a single character
+void ConsumeChar(const u8 **source, u64 *length);
+
+// Disambiguates between Integer, Real32, Real64, and symbol tokens.
+enum TokenType NumberOrSymbolTokenType(const u8 *source, u64 length);
+
+// True if source is pointing at: decimal-point digit* [exponent]
+// if at_least_one_decimal_digit is true, then source must be pointing at:
+//   decimal-point digit+ [exponent] | decimal-point exponent
+b64 IsDecimalAndOptionalExponent(const u8 **source, u64 *length, u8 *exponent_marker, b64 at_least_one_decimal_digit);
+// True if source is pointing at an exponent.
+// Exponent-marker is filled with the Real32 or Real64 exponent marker if true.
+b64 IsExponent(const u8 **source, u64 *length, u8 *exponent_marker);
+
+// True if source is pointing at an integer.
 b64 IsInteger(const u8 *source, u64 length);
+// True if source is pointing at a Real.
+// exponent_marker is filled with the exponent marker of the real (defaulting to 'e').
 b64 IsReal(const u8 *source, u64 length, u8 *exponent_marker);
 
+// True if c is a whitespace character
 b64 IsWhitespace(u8 c);
-b64 IsCloseDelimiter(u8 c);
+// True if c is a close delimiter for a symbol/number
+b64 IsSymbolCloseDelimiter(u8 c);
+// True if marker is a Real32 exponent marker
 b64 IsReal32ExponentMarker(u8 marker);
+// True if marker is a Real exponent marker
 b64 IsExponentMarker(u8 marker);
+// True if c is a number sign
 b64 IsNumberSign(u8 c);
+// True if c is a digit
 b64 IsDigit(u8 c);
 
-struct Token SingleCharacterToken(enum TokenType type, const u8 *source);
+// Result is set to be a single character token. Returns remaining source.
+const u8 *SingleCharacterToken(enum TokenType type, const u8 *source, struct Token *result);
+// Result is set to be a 0-character eof token. Returns remaining source.
+const u8 *EOFToken(const u8 *source, struct Token *result);
+// token is extended to have another character. Returns remaining source.
 const u8 *ExtendToken(const u8 *source, struct Token *token);
+// Returns source after discarding a character.
 const u8 *DiscardChar(const u8 *source);
+
+void TestToken();
 
 const u8 *NextToken(const u8 *source, struct Token *result) {
   source = DiscardInitialWhitespace(source);
   u8 next = *source;
   // Single character tokens
-  if      (next ==  '(')
-    *result = SingleCharacterToken(TOKEN_LIST_OPEN,      source++);
+  if (next ==  '(')
+    return SingleCharacterToken(TOKEN_LIST_OPEN, source, result);
   else if (next ==  ')')
-    *result = SingleCharacterToken(TOKEN_LIST_CLOSE,     source++);
+    return SingleCharacterToken(TOKEN_LIST_CLOSE, source, result);
   else if (next == '\'')
-    *result = SingleCharacterToken(TOKEN_SYMBOLIC_QUOTE, source++);
+    return SingleCharacterToken(TOKEN_SYMBOLIC_QUOTE, source, result);
+  else if (next ==  '.' && IsSymbolCloseDelimiter(*DiscardChar(source)))
+    return SingleCharacterToken(TOKEN_PAIR_SEPARATOR, source, result);
   else if (next == '\0')
-    *result = SingleCharacterToken(TOKEN_EOF,            source);
-  else if (next ==  '.' && IsCloseDelimiter(*DiscardChar(source)))
-    *result = SingleCharacterToken(TOKEN_PAIR_SEPARATOR, source++);
+    return EOFToken(source, result);
 
   // Multi-character tokens
-  else if (next ==  '"') source =        NextStringToken(source, result);
-  else if (next ==  ';') source =    NextLineCommentToken(source, result);
-  else                   source = NextNumberOrSymbolToken(source, result);
-  return source;
+  else if (next ==  '"')
+    return NextStringToken(source, result);
+  else if (next ==  ';')
+    return NextLineCommentToken(source, result);
+  else
+    return NextNumberOrSymbolToken(source, result);
 }
 
 const u8 *HandleEscape(const u8 *source, struct Token *result) {
@@ -88,7 +133,7 @@ const u8 *HandleEscape(const u8 *source, struct Token *result) {
 }
 
 enum TokenType NumberOrSymbolTokenType(const u8 *source, u64 length) {
-  u8 exponent_marker = 'e';
+  u8 exponent_marker;
   if (IsInteger(source, length)) {
     return TOKEN_INTEGER;
   } else if (IsReal(source, length, &exponent_marker)) {
@@ -107,8 +152,8 @@ const u8 *NextNumberOrSymbolToken(const u8 *source, struct Token *result) {
   result->source = source;
   result->length = 0;
   // Token is complete when we encounter a close delimiter
-  for (; !IsCloseDelimiter(*source); source = ExtendToken(source, result))
-    ;
+  for (; !IsSymbolCloseDelimiter(*source); source = ExtendToken(source, result))
+    source = HandleEscape(source, result);
 
   // Determine if integer, real32, real64, or symbol
   result->type = NumberOrSymbolTokenType(result->source, result->length);
@@ -166,18 +211,24 @@ const u8 *TokenTypeString(enum TokenType type) {
   }
 }
 
-struct Token SingleCharacterToken(enum TokenType type, const u8 *source) {
-  struct Token token;
-  token.type = type;
-  token.source = source;
-  token.length = 1;
-  return token;
+const u8 *SingleCharacterToken(enum TokenType type, const u8 *source, struct Token *result) {
+  result->type = type;
+  result->source = source;
+  result->length = 1;
+  return source + 1;
+}
+
+const u8 *EOFToken(const u8 *source, struct Token *result) {
+  result->type = TOKEN_EOF;
+  result->source = source;
+  result->length = 0;
+  return source;
 }
 
 b64 IsWhitespace(u8 ch) { return ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r' || ch == '\v' || ch == '\f'; }
-// Symbols cannot contain "()'; whitespace, or eof
+// Symbols end at "()'; whitespace, or eof
 // Symbols CAN contain .
-b64 IsCloseDelimiter(u8 ch) {
+b64 IsSymbolCloseDelimiter(u8 ch) {
   return IsWhitespace(ch) || ch == '(' || ch == ')' || ch == '\0' || ch == '\'' || ch == '"' || ch == ';';
 }
 
@@ -242,6 +293,7 @@ b64 IsDecimalAndOptionalExponent(const u8 **source, u64 *length, u8 *exponent_ma
 }
 
 b64 IsReal(const u8 *source, u64 length, u8 *exponent_marker) {
+  *exponent_marker = 'e';
   // real := [sign] {digit}*    decimal-point {digit}*   [exponent]
   //       | [sign] {digit}+  [ decimal-point {digit}* ]  exponent
   OptionalNumberSign(&source, &length);
@@ -350,6 +402,15 @@ void TestToken() {
   PrintToken(&token);
 
   source = ";; I'm an unterminated comment";
+  for (source = NextToken(source, &token);
+      token.type != TOKEN_EOF && token.type != TOKEN_UNTERIMINATED_STRING;
+      source = NextToken(source, &token)) {
+    PrintToken(&token);
+    printf("Remaining source: \"%s\"\n", source);
+  }
+  PrintToken(&token);
+
+  source = "symbol\\)-with\\ escaped symbol)split\"bythings\"";
   for (source = NextToken(source, &token);
       token.type != TOKEN_EOF && token.type != TOKEN_UNTERIMINATED_STRING;
       source = NextToken(source, &token)) {
